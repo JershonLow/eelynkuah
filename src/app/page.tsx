7187ef6d12525e736fc78816eccc5ac58fcacd2e
JershonLow <s4023120@student.rmit.edu.au>
2024-11-28 01:10:16 +1100

    Added bio for mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     const [transform, setTransform] = useState(false);
 
     useEffect(() => {
-        setVisible(true); // Trigger the transition on load
+        setVisible(true);
     }, []);
 
     useEffect(() => {
@@ -17,7 +17,8 @@ export default function Home() {
 
     return (
         <div className={"pt-32 h-[100vh] z-0"}>
-            <div className={`hidden sm:flex justify-center flex-row transition-all duration-1000 ease-in-out w-[100vw] 
+            {/*Desktop version*/}
+            <div className={`hidden sm:flex justify-center flex-row transition-temp-1 w-[100vw]
             ${visible ? "opacity-100" : "opacity-0"}
             ${transform ? "translate-y-0" : "translate-y-8"}
             `}>
@@ -38,21 +39,38 @@ export default function Home() {
                             various external and academic projects. I’m eager to apply my creative thinking and
                             management skills in an internship where I can contribute to impactful projects. I am also
                             expecting to learn and grow in my skills and become a contributing member of the
-                            organisation. Do to take a look at my portfolio to see some of the work I&#39;ve done so far,
+                            organisation. Do to take a look at my portfolio to see some of the work I&#39;ve done so
+                            far,
                             and feel free to reach out if you&#39;d like to connect. Thank you for your time! </p>
                     </div>
                 </div>
             </div>
-            <div className={"flex flex-col sm:hidden items-center justify-center"}>
-                <Image
-                    src="/images/IMG_9252_JPG.jpg"
-                    width={450}
-                    height={450}
-                    alt="Picture of the author"
-                    className="circle-image size-[70vw]"
-                />
-                <p className={"text-[20vw] font-primary font-semibold leading-[20vw] w-[70vw]"}>eelyn
-                    kuah</p>
+            {/*Mobile version*/}
+            <div className={`flex flex-col sm:hidden items-center justify-center transition-temp-1
+                ${visible ? "opacity-100" : "opacity-0"}
+                ${transform ? "translate-y-0" : "translate-y-8"}
+                `}>
+                <div className={"flex flex-row"}>
+                    <div className={"pt-3"}>
+                        <p className={"text-[12vw] font-primary font-semibold leading-[13vw] w-[40vw]"}>eelyn kuah</p>
+                        <p className={"primary-text text-[5vw] pb-[4vw] pt-[4vw]"}>my portfolio</p>
+                    </div>
+                    <Image
+                        src="/images/IMG_9252_JPG.jpg"
+                        width={450}
+                        height={450}
+                        alt="Picture of the author"
+                        className="circle-image size-[40vw]"
+                    />
+                </div>
+                <p className={"secondary-text px-[10vw]"}>I am a final-year Communication student at Sunway University
+                    with a strong passion for creating. Over the past few years, I’ve honed my skills in content
+                    creation, social media management, video production as well as event management through
+                    various external and academic projects. I’m eager to apply my creative thinking and
+                    management skills in an internship where I can contribute to impactful projects. I am also
+                    expecting to learn and grow in my skills and become a contributing member of the
+                    organisation. Do to take a look at my portfolio to see some of the work I&#39;ve done so far,
+                    and feel free to reach out if you&#39;d like to connect. Thank you for your time! </p>
             </div>
         </div>
     );
